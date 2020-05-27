c61bfcd3a70ae817adecb98c1a8ccee05791420e
Adam Kochanski <adam.kochanski@schange.com>
2020-05-27 23:02:01 +0200

    Update basic project structure

---
+++ src/components/Search.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Search () {
+  return (
+    <section className="searchBox-wrap">
+      <input type="text" placeholder="Search..." className="searchBox"/>
+    </section>
+  )
+}
+
+export default Search
