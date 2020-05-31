ee93b4a295cf2b18d52f2ce75a7572b3426e145f
Adam Kochanski <adam.kochanski@schange.com>
2020-05-31 13:38:09 +0200

    Prevent empty api calls on pagination

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function App() {
   };
 
   const switchPage = () => {
-    search(undefined, true);
+    if (state.totalPages > 0) { search(undefined, true); }
   };
 
   const handleChange = (page) => {
